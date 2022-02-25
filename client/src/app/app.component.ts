cda1b4790ff49a989f661b2b622f9fcc3df2fc85
maxsuell001 <maxsuell001@outlook.com>
2022-02-24 23:10:02 -0300

    Aba de usuarios

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { AccountService } from './_services/account.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  title: 'SemNome';
   users: any;
 
   constructor(private accountService: AccountService){}
